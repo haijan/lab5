26a2c52003987909d24b527b29f052d364020861
Jan Eric De Castro <jedecast.edu>
2018-02-08 10:33:46 -0800

    Completed Lab 5

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,12 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	
+	$(".friend a").click(function(e) {
+		e.preventDefault();
+		var name = $(this).find("h3");
+		name.text(anagrammedName($(this).text()));
+	})
 }
 
 function anagrammedName(name) {
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,15 @@
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
+	var name = req.query.name;
+	var description = req.query.description;
+	var imageURL = "http://lorempixel.com/400/400/people";
+	data.friends.push(
+		{
+			'name': name,
+			'description': description,
+			'imageURL': imageURL
+		});
+	res.render('index', data);
 	// Your code goes here
  }
